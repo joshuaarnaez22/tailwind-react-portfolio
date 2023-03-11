a7651d0e351fbd826d1525c960f01a986139fde5
Joshua V Arnaez <jarnaez@yondu.com>
2023-03-11 19:51:51 +0800

    portfolio updates

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,8 +1,28 @@
 import Image from "next/image";
-const Card = ({ image }: any) => {
+import Link from "next/link";
+
+const Card = ({ image, techs, link }: any) => {
   return (
-    <div className="">
-      <Image src={image} alt="image" className=" rounded-lg" />
+    <div className="bg-white transition duration-300 ease-in-out hover:scale-90 hover:origin-center hover:rotate-6">
+      <Link href={link} target="_blank">
+        <Image src={image} alt="image" className=" rounded-lg" />
+
+        <div className="bg-gray-100 py-4 ">
+          <div className="container mx-auto px-4">
+            <h2 className="text-3xl font-bold mb-4">Tech Used</h2>
+            <ul className="flex flex-wrap justify-start gap-4 ">
+              {techs &&
+                techs.map((tech: string, index: number) => (
+                  <div key={index}>
+                    <li className="bg-blue-500 text-white font-bold py-2 px-4 rounded-full text-sm uppercase tracking-wide">
+                      {tech}
+                    </li>
+                  </div>
+                ))}
+            </ul>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 };
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
-const Skills = ({ children, image, header, subheader }: any) => {
+const Skills = ({ children, image, header }: any) => {
   return (
     <div className="shadow-2xl text-center p-10 rounded-xl dark:bg-gray-600 md:w-[400px]">
+      <h2 className="dark:text-white text-3xl font-bold mb-4">{header}</h2>
       <Image src={image} alt="image" className="mx-auto" />
       <h3 className="text-lg font-medium pt-8 pb-2 md:text-2xl dark:text-white"></h3>
       <h4 className="py-4 text-teal-600 md:text-xl"></h4>
